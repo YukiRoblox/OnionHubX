362ccb6b95beca50ba4b8c91a4c741d4680284aa
YukiRoblox <138415785+YukiRoblox@users.noreply.github.com>
2023-07-22 14:41:11 -0500

    Update OnionHubX_UI_Testing.lua

--- UI/OnionHubX_UI_Testing.lua
+++ UI/OnionHubX_UI_Testing.lua
@@ -12,7 +12,7 @@ ContentProvider:PreloadAsync({"rbxassetid://3570695787", "rbxassetid://270889159
 
 local Library = {
     Theme = {
-        MainColor = Color3.fromRGB(0, 255, 255),
+        MainColor = Color3.fromRGB(0, 0, 255), -- default color  255, 75, 75
         BackgroundColor = Color3.fromRGB(35, 35, 35),
         UIToggleKey = Enum.KeyCode.RightControl,
         TextFont = Enum.Font.SourceSansBold,
